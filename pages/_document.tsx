71a1740e228087f478d704378101aaad074c9725
lorre huggan <lorre.huggan@protonmail.com>
2022-09-06 17:41:57 +0100

    change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,7 @@ class MyDocument extends Document {
     return (
       <Html data-theme="cupcake">
         <Head>
+          <link rel="icon" type="image/x-icon" href="../public/favicon.png" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"

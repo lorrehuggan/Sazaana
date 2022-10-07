018a52cc3f0b834e981b21b21debfa742ea5fff3
lorre huggan <lorre.huggan@protonmail.com>
2022-10-07 18:48:22 +0100

    chore: update environment

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,6 @@
 const DEV = "http://localhost:3000/";
-const PROD = "https://sazaana-server-production.up.railway.app/";
-const Heroku = "https://sazaana.herokuapp.com/";
-const ENV = DEV;
+const PROD = "https://sazaana.com/";
+const ENV = PROD;
 
 const BASE = `${ENV}api/`;
 //import.meta.env.VITE_AUTH_TOKEN
@@ -19,7 +18,7 @@ export const REFRESH_ENDPOINT = BASE + "main/refresh";
 const DEV_URL = "http://localhost:3000/";
 const PROD_URL = "https://www.sazaana.com/";
 
-const REDIRECT_URL = DEV_URL;
+const REDIRECT_URL = PROD_URL;
 
 export const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=1a98caa930f04397b79800309b24b9c5&response_type=code&redirect_uri=${REDIRECT_URL}&scope=ugc-image-upload%20user-read-recently-played%20user-read-playback-state%20user-top-read%20app-remote-control%20playlist-modify-public%20user-modify-playback-state%20playlist-modify-private%20user-follow-modify%20user-read-currently-playing%20user-follow-read%20user-library-modify%20user-read-playback-position%20playlist-read-private%20user-read-email%20user-read-private%20user-library-read%20playlist-read-collaborative%20streaming&state=state&show_dialog=true`;
 
